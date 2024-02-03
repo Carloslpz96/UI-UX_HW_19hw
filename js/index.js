d5dbb0ba4dadfe0accff4a0143bdbeec6bf86b9b
Carloslpz96 <156606504+Carloslpz96@users.noreply.github.com>
2024-02-03 12:10:40 -0500

    FIXED WORK

--- js/index.js
+++ js/index.js
@@ -1,5 +1,22 @@
 console.log("Your Index.js file is loaded correctly!")
 
-$("#workgrid").mouseenter(function(){
-  alert("Welcome to My Work Section!")
+$( "li.star" ).hover(
+  function() {
+    $( this ).append( $( "<span> <sup>+1</sup> </span>" ) );
+  }, function() {
+    $( this ).find( "span" ).last().remove();
+  }
+);
+
+$( "h1.logotext" ).hover(
+  function() {
+    $( this ).append( $( "<span> <sup>+1</sup> </span>" ) );
+  }, function() {
+    $( this ).find( "span" ).last().remove();
+  }
+);
+ 
+$( "li.fade" ).hover(function() {
+  $( this ).fadeOut( 100 );
+  $( this ).fadeIn( 500 );
 });
